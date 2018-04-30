5b9cdf2868d2a091cef71284dff9bbf3f9a60fd5
SegFault42 <rbouguerra@online.net>
2018-04-30 08:18:36 +0200

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-git clone https://github.com/SegFault42/vim_config /tmp/vim_config
-
 # Install Vundle
 if [ -d "~/.vim/bundle/Vundle.vim" ]; then
+	printf "\033[31mVundle already installed\n\033[0m"
+else
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 	printf "\033[32mInstalling Vundle\n\033[0m"
-else
-	printf "\033[31mVundle already installed\n\033[0m"
 fi
 
-cp -fr /tmp/vim_config/.vimrc ~/.vimrc 
+cp -fr /tmp/vim_config/.vimrc ~/.vimrc
 vim +PluginInstall +qall
 
 #install colorscheme
 mkdir -p ~/.vim/colors
 if [ -d "~/.vim/colors/badwolf.vim" ]; then
-	cp -fr /tmp/vim_config/colors/badwolf.vim ~/.vim/colors/badwolf.vim
-else
 	printf "\033[31mBadwolf already installed\n\033[0m"
+else
+	cp -fr /tmp/vim_config/colors/badwolf.vim ~/.vim/colors/badwolf.vim
 fi
 
 printf "\033[32mInstallation done.\n\033[0m"
