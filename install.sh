04f34c7a1d9192d1b0b0abc1c7335908195ea252
SegFault42 <SegFault42@protonmail.com>
2019-11-06 10:33:21 +0100

    update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/SegFault42/vim_config/ /tmp/vim_config
+git clone https://github.com/SegFault42/PowerVim /tmp/PowerVim
 
 # Install Vundle
 if [ -d "~/.vim/bundle/Vundle.vim" ]; then
@@ -10,7 +10,7 @@ else
 	printf "\033[32mInstalling Vundle\n\033[0m"
 fi
 
-cp -fr /tmp/vim_config/.vimrc ~/.vimrc
+cp -fr /tmp/PowerVim/.vimrc ~/.vimrc
 vim +PluginInstall +qall
 
 printf "\033[32mInstallation done.\n\033[0m"
